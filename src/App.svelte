0eeab1fd30102ba0a2bbc30c6822021f0b2681c7
RedPhoenixQ <steve.dark.03@gmail.com>
2024-01-30 15:42:48 +0100

    batman

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main
+  class="grid grid-cols-5 grid-rows-4 gap-1 [&>div]:border-2 [&>div]:border-black"
+>
+  <h1 class="col-span-full text-lg">Business Model Canvas</h1>
+
+  <div class="col-start-1 row-span-2 bg-cyan-600"></div>
+  <div class="col-start-2 bg-cyan-500"></div>
+  <div class="col-start-2 row-start-3 bg-cyan-500"></div>
+
+  <div
+    class="col-start-3 row-span-3 row-start-2 flex items-center justify-center bg-yellow-400"
+  >
+    <Counter />
+  </div>
+
+  <div class="col-start-4 bg-lime-500"></div>
+  <div class="col-start-4 row-start-3 bg-lime-500"></div>
+  <div class="col-start-5 row-span-2 bg-lime-600"></div>
+
+  <div class="col-span-2 bg-amber-500"></div>
+  <div class="col-span-2 bg-amber-500"></div>
+</main>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from "path";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), viteSingleFile({
+    removeViteModuleLoader: true,
+  })],
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib")
+    }
+  }
+})
