53ab5c580caecb203d85b22df086b5f61ef30b2c
RedPhoenixQ <steve.dark.03@gmail.com>
2024-01-31 10:18:15 +0100

    New better styling

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,50 @@
 </script>
 
 <main
-  class="grid grid-cols-5 grid-rows-4 gap-1 [&>div]:border-2 [&>div]:border-black"
+  class="grid min-h-screen grid-rows-3 gap-1 bg-neutral-600 p-2 [&_section]:border-2 [&_section]:border-black"
+  style="grid-template-columns: 2fr 1fr 2fr; grid-template-rows: auto 3fr 1fr;"
 >
-  <h1 class="col-span-full text-lg">Business Model Canvas</h1>
+  <h1 class="col-span-full text-lg font-bold text-white">
+    Business Model Canvas
+  </h1>
 
-  <div class="col-start-1 row-span-2 bg-cyan-600"></div>
-  <div class="col-start-2 bg-cyan-500"></div>
-  <div class="col-start-2 row-start-3 bg-cyan-500"></div>
+  <section class="flex flex-col bg-cyan-600">
+    <h2 class="p-2 text-lg font-bold text-white">Key Partners</h2>
+    <div class="grid flex-grow grid-cols-2">
+      <div class="row-span-2"></div>
+      <section class="bg-cyan-500">
+        <h3 class="text-md p-2 font-bold text-white">Key Activites</h3>
+      </section>
+      <section class="bg-cyan-500">
+        <h3 class="text-md p-2 font-bold text-white">Key Resources</h3>
+      </section>
+    </div>
+  </section>
 
-  <div
-    class="col-start-3 row-span-3 row-start-2 flex items-center justify-center bg-yellow-400"
-  >
-    <Counter />
-  </div>
+  <section class="row-span-2 flex flex-col bg-yellow-400">
+    <h2 class="p-2 text-lg font-bold text-neutral-700">Value Propositions</h2>
+    <div class="flex flex-grow items-center justify-center">
+      <Counter />
+    </div>
+  </section>
 
-  <div class="col-start-4 bg-lime-500"></div>
-  <div class="col-start-4 row-start-3 bg-lime-500"></div>
-  <div class="col-start-5 row-span-2 bg-lime-600"></div>
+  <section class="flex flex-col bg-lime-600">
+    <h2 class="p-2 text-lg font-bold text-white">Customer Segments</h2>
+    <div class="grid flex-grow grid-cols-2">
+      <div class="row-span-2"></div>
+      <section class="bg-lime-500">
+        <h3 class="text-md p-2 font-bold text-white">Customer Relationships</h3>
+      </section>
+      <section class="bg-lime-500">
+        <h3 class="text-md p-2 font-bold text-white">Channels</h3>
+      </section>
+    </div>
+  </section>
 
-  <div class="col-span-2 bg-amber-500"></div>
-  <div class="col-span-2 bg-amber-500"></div>
+  <section class="bg-amber-500">
+    <h2 class="p-2 text-lg font-bold text-white">Cost Structure</h2>
+  </section>
+  <section class="bg-amber-500">
+    <h2 class="p-2 text-lg font-bold text-white">Revenue Streams</h2>
+  </section>
 </main>
