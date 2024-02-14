f621074134175039a18239de9c9ef510ccb9c0a2
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-14 14:30:27 +0100

    Add access to the position the menu was opened at

--- src/lib/CustomContextMenuTrigger.svelte
+++ src/lib/CustomContextMenuTrigger.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
   import * as ContextMenu from "$lib/components/ui/context-menu";
+  import type { XYPosition } from "@xyflow/svelte";
 
   let open = false;
+
+  export let opened_at: XYPosition = { x: 0, y: 0 };
+
+  function handleContextMenu(
+    event: CustomEvent<{ originalEvent: PointerEvent }>,
+  ) {
+    if (event.detail.originalEvent.defaultPrevented) {
+      event.preventDefault();
+      open = false;
+    }
+    opened_at.x = event.detail.originalEvent.clientX;
+    opened_at.y = event.detail.originalEvent.clientY;
+    event.detail.originalEvent.preventDefault();
+  }
 </script>
 
 <ContextMenu.Root bind:open>
@@ -9,12 +24,7 @@
     {...$$restProps}
     on:contextmenu={(event) => {
       // @ts-ignore: ContextMenu event is improperly typed
-      if (event.detail.originalEvent.defaultPrevented) {
-        event.preventDefault();
-        open = false;
-      }
-      // @ts-ignore: ContextMenu event is improperly typed
-      event.detail.originalEvent.preventDefault();
+      handleContextMenu(event);
     }}
   >
     <slot />
