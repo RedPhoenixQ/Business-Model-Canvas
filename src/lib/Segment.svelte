4974535db4671af28489ca1f2b6bd3980374599c
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-18 17:15:10 +0100

    Fix resizing by not caching node object
    The node object might change and not be updated, so we get it every time we need it instead

--- src/lib/Segment.svelte
+++ src/lib/Segment.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
-  import { type NodeProps, NodeResizeControl, useStore } from "@xyflow/svelte";
+  import {
+    type NodeProps,
+    NodeResizeControl,
+    useUpdateNodeInternals,
+    useSvelteFlow,
+  } from "@xyflow/svelte";
   import {
     segmentInfo,
     type SegmentData,
@@ -12,49 +17,69 @@
 
   export let id: $$Props["id"];
 
-  const { nodeLookup, domNode, updateNodePositions } = useStore();
+  const { getNode } = useSvelteFlow();
+  const updateNodeInternals = useUpdateNodeInternals();
 
   const { title, colorClass, grid, size } = segmentInfo[id as SegmentKey];
   const cols = grid.column.end - grid.column.start;
   const rows = grid.row.end - grid.row.start;
 
-  $: nodeElement = $domNode?.querySelector(
-    `.svelte-flow__node[data-id="${id}"]`,
-  ) as HTMLDivElement | undefined;
-  $: node = $nodeLookup.get(id);
-
-  $: if (node && nodeElement?.style) {
-    node.width = 0;
-    for (let i = grid.column.start; i < grid.column.end; i++) {
-      node.width += $segmentColumns[i];
+  $: {
+    const node = getNode(id);
+    if (node) {
+      let new_width = 0;
+      for (let i = grid.column.start; i < grid.column.end; i++) {
+        new_width += $segmentColumns[i];
+      }
+      let new_height = 0;
+      for (let i = grid.row.start; i < grid.row.end; i++) {
+        new_height += $segmentRows[i];
+      }
+      let new_x = 0;
+      for (let i = 0; i < grid.column.start; i++) {
+        new_x += $segmentColumns[i];
+      }
+      let new_y = -new_height;
+      for (let i = 0; i < grid.row.start; i++) {
+        new_y -= $segmentRows[i];
+      }
+      if (
+        node.position.x !== new_x ||
+        node.position.y !== new_y ||
+        node.width !== new_width ||
+        node.height !== new_height
+      ) {
+        node.position.x = new_x;
+        node.position.y = new_y;
+        node.width = new_width;
+        node.height = new_height;
+        updateNodeInternals(id);
+      }
     }
-    node.height = 0;
-    for (let i = grid.row.start; i < grid.row.end; i++) {
-      node.height += $segmentRows[i];
-    }
-    let new_x = 0;
-    for (let i = 0; i < grid.column.start; i++) {
-      new_x += $segmentColumns[i];
-    }
-    let new_y = -node.height;
-    for (let i = 0; i < grid.row.start; i++) {
-      new_y -= $segmentRows[i];
-    }
-
-    if (node.position.x !== new_x || node.position.y !== new_y) {
-      node.position.x = new_x;
-      node.position.y = new_y;
-      updateNodePositions([node]);
-    }
-    // Manual size update because the nodes don't update automatically
-    nodeElement.style.width = node.width + "px";
-    nodeElement.style.height = node.height + "px";
   }
 
-  function onResize() {
-    if (!node?.width || !node?.height) return;
-    const dx = offsetWidth - node.width;
-    const dy = offsetHeight - node.height;
+  let prevWidth: number | null | undefined;
+  let prevHeight: number | null | undefined;
+  function onResizeStart() {
+    const node = getNode(id);
+    if (!node) {
+      prevHeight = null;
+      prevWidth = null;
+    } else {
+      prevWidth = node.width;
+      prevHeight = node.height;
+    }
+  }
+  function onResizeEnd() {
+    const node = getNode(id);
+    if (!node?.width || !node?.height || !prevWidth || !prevHeight) return;
+    const dx = node.width - prevWidth;
+    const dy = node.height - prevHeight;
+    console.log("resize prev", prevWidth, prevHeight);
+    console.log("resize", dx, dy);
+    // TODO:  Find a better way of distributing the size increase.
+    //        Should probably move a "grid line" instread of a block
+    //        or respect minimum size of grid blocks
     for (let i = grid.column.start; i < grid.column.end; i++) {
       $segmentColumns[i] += dx / cols;
     }
@@ -62,14 +87,9 @@
       $segmentRows[i] += dy / rows;
     }
   }
-
-  let offsetWidth: number;
-  let offsetHeight: number;
 </script>
 
 <div
-  bind:offsetWidth
-  bind:offsetHeight
   class="h-full w-full border-2 border-white border-opacity-25 bg-gray-700 bg-opacity-50 {colorClass} {$$restProps?.class ??
     ''}"
 >
@@ -79,8 +99,8 @@
     minWidth={size.width}
     position="top-right"
     style="background: none; border: none;"
-    {onResize}
-    onResizeEnd={onResize}
+    {onResizeStart}
+    {onResizeEnd}
   >
     <svg
       class="pointer-events-auto aspect-square w-6 -translate-x-full translate-y-1 stroke-black p-1 dark:stroke-white"
