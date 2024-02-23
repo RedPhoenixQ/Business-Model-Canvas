539bea411667c6225864bdaf6e439b13f7c1cedd
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-23 17:42:33 +0100

    Add debug only menu for testing

---
+++ src/lib/menubar/Debug.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import * as Menubar from "$lib/components/ui/menubar";
+  import { useEdges, useNodes } from "@xyflow/svelte";
+
+  const nodes = useNodes();
+  const edges = useEdges();
+</script>
+
+<Menubar.Menu>
+  <Menubar.Trigger>Debug</Menubar.Trigger>
+  <Menubar.Content>
+    <Menubar.Item
+      on:click={() => {
+        $edges.forEach((edge) => {
+          edge.hidden = false;
+        });
+        $edges = $edges;
+      }}
+    >
+      Show all edges
+    </Menubar.Item>
+  </Menubar.Content>
+</Menubar.Menu>
--- src/lib/menubar/Menubar.svelte
+++ src/lib/menubar/Menubar.svelte
@@ -2,10 +2,14 @@
   import * as Menubar from "$lib/components/ui/menubar";
   import File from "./File.svelte";
   import Settings from "./Settings.svelte";
+  import Debug from "./Debug.svelte";
 </script>
 
 <Menubar.Root>
   <slot />
   <File />
   <Settings />
+  {#if import.meta.env.DEV}
+    <Debug />
+  {/if}
 </Menubar.Root>
