234158adb72837389cc5fc60f2e4e2e4aeb020f6
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-24 01:49:05 +0100

    Add Edit menu with undo/redo and shortcuts

---
+++ src/lib/menubar/Edit.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { UndoIcon, RedoIcon } from "lucide-svelte";
+  import * as Menubar from "$lib/components/ui/menubar";
+  import { useHistory, undoEmpty, redoEmpty } from "$lib/history";
+
+  const { undo, redo, applyHistory } = useHistory();
+
+  function shortcuts(event: KeyboardEvent) {
+    console.log(event);
+    switch (event.code) {
+      case "KeyZ":
+        if (!event.ctrlKey) return;
+        applyHistory(!event.shiftKey);
+        break;
+      case "KeyY":
+        if (!event.ctrlKey) return;
+        redo();
+        break;
+
+      default:
+        return;
+    }
+    event.preventDefault();
+  }
+</script>
+
+<svelte:document on:keydown={shortcuts} />
+
+<Menubar.Menu>
+  <Menubar.Trigger>Edit</Menubar.Trigger>
+  <Menubar.Content class="[&_[role=menuitem]]:gap-2">
+    <Menubar.Item on:click={undo} disabled={$undoEmpty}>
+      <UndoIcon size="20" />
+      Undo
+      <Menubar.Shortcut>^Z</Menubar.Shortcut>
+    </Menubar.Item>
+    <Menubar.Item on:click={redo} disabled={$redoEmpty}>
+      <RedoIcon size="20" />
+      Redo
+      <Menubar.Shortcut>^Y</Menubar.Shortcut>
+    </Menubar.Item>
+  </Menubar.Content>
+</Menubar.Menu>
