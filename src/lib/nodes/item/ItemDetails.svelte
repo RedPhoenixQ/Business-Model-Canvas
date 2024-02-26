51789b19986b93f35c4c4436f7b91f1a0bddd942
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-26 20:14:41 +0100

    Make icon settings changable via popover

--- src/lib/nodes/item/ItemDetails.svelte
+++ src/lib/nodes/item/ItemDetails.svelte
@@ -7,8 +7,22 @@
   import ItemIcon from "./ItemIcon.svelte";
   import { itemDetails, type ItemData } from ".";
   import { addHistoryEntry } from "$lib/project/history";
+  import IconSearch from "./IconSearch.svelte";
+  import * as Popover from "$lib/components/ui/popover";
+  import * as Select from "$lib/components/ui/select";
+  import { EditIcon } from "lucide-svelte";
 
   const { updateNodeData } = useSvelteFlow();
+
+  const iconBackground = [
+    { label: "None", value: "none" },
+    { label: "Light", value: "light" },
+    { label: "Dark", value: "dark" },
+  ];
+  const iconShape = [
+    { label: "Square", value: "square" },
+    { label: "Circle", value: "circle" },
+  ];
 
   function change() {
     if (
@@ -85,7 +99,67 @@
             bind:value={data.icon.src}
             on:change={change}
           />
-          <ItemIcon icon={data.icon} alt={data.name} />
+          <Popover.Root>
+            <Popover.Trigger class="relative rounded">
+              <ItemIcon icon={data.icon} alt={data.name} />
+              <div
+                class="absolute right-0 top-0 flex h-full w-full items-center justify-center bg-muted/25 p-4 text-black opacity-0 transition-opacity hover:opacity-100"
+              >
+                <EditIcon class="text-white" />
+              </div>
+            </Popover.Trigger>
+            <Popover.Content
+              side="left"
+              sideOffset={20}
+              class="flex flex-col gap-4"
+            >
+              <div>Edit Icon</div>
+              <Select.Root
+                items={iconBackground}
+                selected={iconBackground.find(
+                  ({ value }) => value === data?.icon?.background,
+                ) ?? iconBackground[0]}
+                onSelectedChange={(selected) => {
+                  if (!data || !selected) return;
+                  const value = selected.value;
+                  data.icon.background =
+                    value === "light" || value === "dark" ? value : undefined;
+                }}
+              >
+                <Select.Trigger>
+                  <Select.Value class="capitalize" />
+                </Select.Trigger>
+                <Select.Content>
+                  {#each iconBackground as { value, label }}
+                    <Select.Item {value}>{label}</Select.Item>
+                  {/each}
+                </Select.Content>
+              </Select.Root>
+              <Select.Root
+                items={iconShape}
+                selected={iconShape.find(
+                  ({ value }) => value === data?.icon?.shape,
+                ) ?? iconShape[0]}
+                onSelectedChange={(selected) => {
+                  if (!data || !selected) return;
+                  const value = selected.value;
+                  data.icon.shape =
+                    value === "circle" || value === "square"
+                      ? value
+                      : undefined;
+                }}
+              >
+                <Select.Trigger>
+                  <Select.Value class="capitalize" />
+                </Select.Trigger>
+                <Select.Content>
+                  {#each iconShape as { value, label }}
+                    <Select.Item {value}>{label}</Select.Item>
+                  {/each}
+                </Select.Content>
+              </Select.Root>
+            </Popover.Content>
+          </Popover.Root>
         </div>
       </div>
     {:else}
