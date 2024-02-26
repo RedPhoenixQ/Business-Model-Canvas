8adfdfc7ef3db7aab1be04ab37bb7371ac4a73fd
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-26 13:19:23 +0100

    Make target handle invisibly cover the entire item

--- src/lib/nodes/item/Item.svelte
+++ src/lib/nodes/item/Item.svelte
@@ -1,49 +1,24 @@
 <script lang="ts">
-  import * as ContextMenu from "$lib/components/ui/context-menu";
-  import {
-    Handle,
-    Position,
-    type NodeProps,
-    useSvelteFlow,
-  } from "@xyflow/svelte";
-  import { itemDetails, type ItemData } from ".";
+  import ItemContextMenu from "./ItemContextMenu.svelte";
+  import { Handle, Position, type NodeProps } from "@xyflow/svelte";
+  import type { ItemData } from ".";
   import ItemIcon from "./ItemIcon.svelte";
   import CustomContextMenuTrigger from "../../CustomContextMenuTrigger.svelte";
-  import { addHistoryEntry } from "$lib/project/history";
 
   type $$Props = NodeProps<ItemData>;
 
   export let id: $$Props["id"];
   export let data: $$Props["data"];
   export let selected: $$Props["selected"];
-
-  const { deleteElements } = useSvelteFlow();
-
-  async function deleteNode(id: string) {
-    const deleted = await deleteElements({ nodes: [{ id }] });
-    addHistoryEntry({
-      type: "delete",
-      nodes: deleted.deletedNodes,
-      edges: deleted.deletedEdges,
-    });
-  }
 </script>
 
 <CustomContextMenuTrigger>
-  <div class="aspect-square w-10 {selected ? 'scale-125' : ''}">
+  <div class="relative aspect-square w-10 {selected ? 'scale-125' : ''}">
+    <Handle type="source" position={Position.Top} />
     <Handle
+      style="top: 0; left: 0; transform: none; width: 100%; height: 100%; visibility: hidden;"
       type="target"
-      onconnect={(connections) => {
-        console.log(connections);
-      }}
       position={Position.Top}
-    />
-    <Handle
-      type="source"
-      onconnect={(connections) => {
-        console.log(connections);
-      }}
-      position={Position.Bottom}
     />
     <ItemIcon src={data.icon} alt={data.name} />
   </div>
