34fa388532b54fe2d490234f02b1bcf9f155d74f
RedPhoenixQ <steve.dark.03@gmail.com>
2024-02-26 15:11:13 +0100

    Create styled toolbar components

---
+++ src/lib/components/custom/toolbar/index.ts
@@ -0,0 +1,13 @@
+import Root from "./toolbar.svelte";
+import Button from "./toolbar-button.svelte";
+import Separator from "./toolbar-separator.svelte";
+
+export {
+  Root,
+  Button,
+  Separator,
+  //
+  Root as Toolbar,
+  Button as ToolbarButton,
+  Separator as ToolbarSeparator,
+};
---
+++ src/lib/components/custom/toolbar/toolbar-separator.svelte
@@ -0,0 +1,5 @@
+<script>
+  import { Separator } from "bits-ui";
+</script>
+
+<Separator.Root class="-my-1 mx-1 w-[1px] self-stretch bg-muted" />
