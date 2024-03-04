b8aa53dfaa60fda112f41cf9eae3ac3577162a11
Alleops <alexanderhavren@live.com>
2024-03-04 14:39:02 +0100

    Add Slider NodeType

--- src/lib/nodes/index.ts
+++ src/lib/nodes/index.ts
@@ -1,9 +1,11 @@
 import type { NodeTypes } from "@xyflow/svelte";
 import Item from "./item/Item.svelte";
 import Segment from "./segment/Segment.svelte";
+import Slider from "./slider/Slider.svelte";
 
 export const nodeTypes: NodeTypes = {
   item: Item,
   text: Item,
   segment: Segment,
+  slider: Slider,
 };
---
+++ src/lib/nodes/slider/Slider.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { type NodeProps } from "@xyflow/svelte";
+  import Slider from "$lib/components/ui/slider/slider.svelte";
+  import MaleIcon from "./MaleIcon.svelte";
+  import FemaleIcon from "./FemaleIcon.svelte";
+
+  type $$Props = NodeProps<{}>;
+
+  export let id: $$Props["id"];
+  export let data: $$Props["data"];
+</script>
+
+<div class="grid gap-2" style:grid-template-columns="auto 1fr auto">
+  <MaleIcon />
+  <Slider value={[50]} min={0} max={100} class="w-24" />
+  <FemaleIcon />
+</div>
