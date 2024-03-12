feebe7228c2d5c9ca3bc54419f1a698ea28c9735
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-13 00:08:32 +0100

    Clamp min resize to most bottom right contained node

--- src/lib/nodes/ResizeControl.svelte
+++ src/lib/nodes/ResizeControl.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import ResizeIcon from "$lib/icons/ResizeIcon.svelte";
-  import { NodeResizeControl, useSvelteFlow } from "@xyflow/svelte";
+  import {
+    NodeResizeControl,
+    useSvelteFlow,
+    type XYPosition,
+    useNodes,
+  } from "@xyflow/svelte";
 
   type $$Props = NodeResizeControl["$$prop_def"] & {
     id: string;
@@ -9,8 +14,29 @@
   export let id: string;
 
   const { getNode } = useSvelteFlow();
+  const nodes = useNodes();
   /** The minimum distance from a node parent */
   const resizePadding = 5 as const;
+
+  let maxContainedNode: XYPosition | undefined;
+  function findMaxContainedNode(): XYPosition {
+    const max: XYPosition = {
+      x: 0,
+      y: 0,
+    };
+    for (const node of $nodes) {
+      if (node.parentNode !== id) continue;
+      max.x = Math.max(
+        max.x,
+        node.position.x + (node.width ?? node.computed?.width ?? 0),
+      );
+      max.y = Math.max(
+        max.y,
+        node.position.y + (node.height ?? node.computed?.height ?? 0),
+      );
+    }
+    return max;
+  }
 </script>
 
 <NodeResizeControl
@@ -18,6 +44,11 @@
   position="bottom-right"
   style="background: none; border: none;"
   shouldResize={(_, { direction, width, height, x, y }) => {
+    if (!maxContainedNode) {
+      maxContainedNode = findMaxContainedNode();
+    }
+    console.log(maxContainedNode);
+
     const node = getNode(id);
     if (!node?.parentNode) return true;
     const parent = getNode(node?.parentNode);
@@ -25,14 +56,20 @@
       return true;
     if (
       (direction[0] !== 0 &&
-        parent.width - resizePadding < resizePadding + x + width) ||
+        (parent.width - resizePadding < resizePadding + x + width ||
+          width - resizePadding < maxContainedNode.x + resizePadding)) ||
       (direction[1] !== 0 &&
-        parent.height - resizePadding < resizePadding + y + height)
+        (parent.height - resizePadding < resizePadding + y + height ||
+          height - resizePadding < maxContainedNode.y + resizePadding))
     ) {
       return false;
     }
     // TODO: Check for items inside aswell
     return true;
+  }}
+  onResizeEnd={(event) => {
+    maxContainedNode = undefined;
+    $$props?.onResizeEnd?.(event);
   }}
 >
   <ResizeIcon
