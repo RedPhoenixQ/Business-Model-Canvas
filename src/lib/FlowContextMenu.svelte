5189678f766dcb7d6462858b39faf8205b361393
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-12 15:04:54 +0100

    Use defaultNodes when creating new nodes

--- src/lib/FlowContextMenu.svelte
+++ src/lib/FlowContextMenu.svelte
@@ -7,19 +7,16 @@
     type XYPosition,
     useNodes,
   } from "@xyflow/svelte";
-  import type { ItemData } from "./nodes/item";
-  import type { GroupData } from "./nodes/group";
-  import type { SliderData } from "./nodes/slider";
   import { addHistoryEntry } from "./project/history";
   import { PlusIcon } from "lucide-svelte";
-  import type { nodeTypes } from "./nodes";
+  import { defaultNodes } from "./nodes";
 
   export let opened_at: XYPosition;
 
   const nodes = useNodes();
   const { screenToFlowPosition, getIntersectingNodes } = useSvelteFlow();
 
-  function addItem(any_event: any, type: keyof typeof nodeTypes) {
+  function addItem(any_event: any, type: keyof typeof defaultNodes) {
     // Event type from bits-ui handlers is incorrect so we force the type
     const event = any_event as CustomEvent<{
       originalEvent: PointerEvent;
@@ -53,42 +50,12 @@
       };
     }
 
-    const node: Node<unknown, keyof typeof nodeTypes> = {
-      type,
+    let node: Node = {
+      ...structuredClone(defaultNodes[type]),
       id: crypto.randomUUID() as string,
-      position,
-      data: undefined,
-      extent: "parent",
+      position: position,
       parentNode: parent?.id,
     };
-    switch (type) {
-      case "item":
-        node.data = {
-          name: "",
-          icon: {},
-          description: "",
-          showText: false,
-          showTitle: false,
-          textHeight: 80,
-          textWidth: 120,
-        } satisfies ItemData;
-        break;
-      case "slider":
-        node.data = {
-          value: [50],
-        } satisfies SliderData;
-        break;
-      case "customGroup":
-        node.zIndex = -5;
-        node.width = 60;
-        node.height = 60;
-        node.data = {
-          title: "Test",
-        } satisfies GroupData;
-        break;
-      default:
-        break;
-    }
     $nodes.push(node);
     $nodes = $nodes;
     addHistoryEntry({ type: "createNode", node });
