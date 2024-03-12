a1d58717cae1bb143915f6a534e077d722a64ae1
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-12 14:27:23 +0100

    Make node types a constant
    This allows for types to know all possible node types as string literals

--- src/lib/nodes/index.ts
+++ src/lib/nodes/index.ts
@@ -3,8 +3,8 @@ import Item from "./item/Item.svelte";
 import Segment from "./segment/Segment.svelte";
 import Slider from "./slider/Slider.svelte";
 
-export const nodeTypes: NodeTypes = {
+export const nodeTypes = {
   item: Item,
   segment: Segment,
   slider: Slider,
-};
+} as const satisfies NodeTypes;
