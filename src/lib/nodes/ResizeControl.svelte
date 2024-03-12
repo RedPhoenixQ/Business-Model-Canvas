a03b7b36fe226af5e5ac7c4aacddef8ffdd8b09a
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-13 00:46:37 +0100

    Improve boudary system by using min and max props

--- src/lib/nodes/ResizeControl.svelte
+++ src/lib/nodes/ResizeControl.svelte
@@ -1,78 +1,97 @@
 <script lang="ts">
   import ResizeIcon from "$lib/icons/ResizeIcon.svelte";
-  import {
-    NodeResizeControl,
-    useSvelteFlow,
-    type XYPosition,
-    useNodes,
-  } from "@xyflow/svelte";
+  import { NodeResizeControl, useSvelteFlow, useNodes } from "@xyflow/svelte";
 
   type $$Props = NodeResizeControl["$$prop_def"] & {
     id: string;
   };
 
-  export let id: string;
+  export let id: $$Props["id"];
+  let _minWidth: $$Props["minWidth"] = 10;
+  let _minHeight: $$Props["minHeight"] = 10;
+  let _maxWidth: $$Props["maxWidth"] = Number.MAX_VALUE;
+  let _maxHeight: $$Props["maxHeight"] = Number.MAX_VALUE;
+  export {
+    _minWidth as minWidth,
+    _minHeight as minHeight,
+    _maxWidth as maxWidth,
+    _maxHeight as maxHeight,
+  };
 
   const { getNode } = useSvelteFlow();
   const nodes = useNodes();
   /** The minimum distance from a node parent */
-  const resizePadding = 5 as const;
+  const resizePadding = 20 as const;
 
-  let maxContainedNode: XYPosition | undefined;
-  function findMaxContainedNode(): XYPosition {
-    const max: XYPosition = {
-      x: 0,
-      y: 0,
-    };
+  let minWidth: number;
+  let minHeight: number;
+  let maxWidth: number;
+  let maxHeight: number;
+  /**
+   * Sets the minimum size to be outside the outermost contained node and
+   * the maximun size to be the remaining space in the containing node, if any
+   */
+  function updateBoundaries() {
+    // Reset boundaries
+    minWidth = _minWidth ?? 10;
+    minHeight = _minHeight ?? 10;
+    maxWidth = _maxWidth ?? Number.MAX_VALUE;
+    maxHeight = _maxHeight ?? Number.MAX_VALUE;
+
     for (const node of $nodes) {
       if (node.parentNode !== id) continue;
-      max.x = Math.max(
-        max.x,
+      minWidth = Math.max(
+        minWidth,
         node.position.x + (node.width ?? node.computed?.width ?? 0),
       );
-      max.y = Math.max(
-        max.y,
+      minHeight = Math.max(
+        minHeight,
         node.position.y + (node.height ?? node.computed?.height ?? 0),
       );
     }
-    return max;
+    minWidth += resizePadding;
+    minHeight += resizePadding;
+
+    const node = getNode(id);
+    if (!node?.parentNode) return;
+    const parent = getNode(node?.parentNode);
+    if (!parent) {
+      return;
+    }
+    maxWidth = Math.min(
+      (parent.width ?? parent.computed?.width ?? 0) -
+        node.position.x -
+        resizePadding,
+      maxWidth,
+    );
+    maxHeight = Math.min(
+      (parent.height ?? parent.computed?.height ?? 0) -
+        node.position.y -
+        resizePadding,
+      maxHeight,
+    );
   }
 </script>
 
-<NodeResizeControl
-  {...$$props}
-  position="bottom-right"
-  style="background: none; border: none;"
-  shouldResize={(_, { direction, width, height, x, y }) => {
-    if (!maxContainedNode) {
-      maxContainedNode = findMaxContainedNode();
-    }
-    console.log(maxContainedNode);
-
-    const node = getNode(id);
-    if (!node?.parentNode) return true;
-    const parent = getNode(node?.parentNode);
-    if (typeof parent?.width !== "number" || typeof parent?.height !== "number")
-      return true;
-    if (
-      (direction[0] !== 0 &&
-        (parent.width - resizePadding < resizePadding + x + width ||
-          width - resizePadding < maxContainedNode.x + resizePadding)) ||
-      (direction[1] !== 0 &&
-        (parent.height - resizePadding < resizePadding + y + height ||
-          height - resizePadding < maxContainedNode.y + resizePadding))
-    ) {
-      return false;
-    }
-    // TODO: Check for items inside aswell
-    return true;
-  }}
-  onResizeEnd={(event) => {
-    maxContainedNode = undefined;
-    $$props?.onResizeEnd?.(event);
+<div
+  class="contents"
+  on:pointerdown={() => {
+    // Update boundaries before any drag event fires
+    // This ensures that drag handlers use the newest boundaries
+    updateBoundaries();
   }}
 >
-  <ResizeIcon
-    class="pointer-events-auto -translate-x-full -translate-y-full opacity-50 transition-opacity hover:opacity-100"
-  />
-</NodeResizeControl>
+  <NodeResizeControl
+    {...$$props}
+    {minWidth}
+    {minHeight}
+    {maxWidth}
+    {maxHeight}
+    position="bottom-right"
+    style="background: none; border: none;"
+  >
+    <ResizeIcon
+      class="pointer-events-auto -translate-x-full -translate-y-full opacity-50 transition-opacity hover:opacity-100"
+    />
+  </NodeResizeControl>
+</div>
