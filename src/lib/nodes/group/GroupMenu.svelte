fa8231da1e49c1f7633c579c0949b80bcca38c21
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-19 00:27:43 +0100

    Add Delete to GroupMenu

--- src/lib/nodes/group/GroupMenu.svelte
+++ src/lib/nodes/group/GroupMenu.svelte
@@ -2,11 +2,12 @@
   import { TestTubeIcon } from "lucide-svelte";
   import { menu, type MenuType } from "$lib/components/custom/menu";
   import MoveMenuPart from "../MoveMenuPart.svelte";
+  import DeleteNodeMenuPart from "../DeleteNodeMenuPart.svelte";
 
   export let type: MenuType;
   export let id: string;
 
-  const { Content, Item } = menu(type);
+  const { Content, Item, Separator } = menu(type);
 </script>
 
 <Content {...$$restProps}>
@@ -15,4 +16,6 @@
     <TestTubeIcon size={20} class="mr-2" />
     Test
   </Item>
+  <Separator />
+  <DeleteNodeMenuPart {type} {id} />
 </Content>
