0364808d821305fe5b61f66e09bec12735d86726
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-19 00:29:09 +0100

    Remove Test Item from GroupMenu

--- src/lib/nodes/group/GroupMenu.svelte
+++ src/lib/nodes/group/GroupMenu.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { TestTubeIcon } from "lucide-svelte";
   import { menu, type MenuType } from "$lib/components/custom/menu";
   import MoveMenuPart from "../MoveMenuPart.svelte";
   import DeleteNodeMenuPart from "../DeleteNodeMenuPart.svelte";
@@ -7,15 +6,11 @@
   export let type: MenuType;
   export let id: string;
 
-  const { Content, Item, Separator } = menu(type);
+  const { Content, Separator } = menu(type);
 </script>
 
 <Content {...$$restProps}>
   <MoveMenuPart {type} {id} />
-  <Item on:click={console.log}>
-    <TestTubeIcon size={20} class="mr-2" />
-    Test
-  </Item>
   <Separator />
   <DeleteNodeMenuPart {type} {id} />
 </Content>
