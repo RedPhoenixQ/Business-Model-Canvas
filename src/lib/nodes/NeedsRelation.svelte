98e06d41182dc8561d80bd7a108dfaa0c035d46f
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-23 17:31:05 +0100

    Update NeedsRelation to update data and show more usefull information

--- src/lib/nodes/NeedsRelation.svelte
+++ src/lib/nodes/NeedsRelation.svelte
@@ -1,34 +1,70 @@
 <script lang="ts">
   import { templateInfoStore } from "$lib/project";
+  import { lastEntry } from "$lib/project/history";
   import { getConnectedEdges, useEdges, useSvelteFlow } from "@xyflow/svelte";
-  import { InfoIcon } from "lucide-svelte";
+  import { AlertCircleIcon, CheckCircle2Icon } from "lucide-svelte";
 
   export let id: string;
 
-  const { getIntersectingNodes, getNode } = useSvelteFlow();
+  const { getNode } = useSvelteFlow();
   const edges = useEdges();
 
-  $: missingRelations = findMissingRelations(id);
+  $: relations = id ? findMissingRelations() : undefined;
+  $: {
+    switch ($lastEntry?.type) {
+      case "move":
+        if (thisAndParentIds.includes($lastEntry.id) && $lastEntry.parent) {
+          relations = findMissingRelations();
+        }
+        break;
+      case "create":
+      case "delete":
+        if (
+          ($lastEntry.edges &&
+            $lastEntry.edges.some(
+              (edge) =>
+                thisAndParentIds.includes(edge.source) ||
+                thisAndParentIds.includes(edge.target),
+            )) ||
+          ($lastEntry.nodes && $lastEntry.nodes.some((node) => node.id === id))
+        ) {
+          console.log("history relation change", $lastEntry);
+          relations = findMissingRelations();
+        }
+        break;
+    }
+  }
 
-  function findMissingRelations(id: string): string[][] | undefined {
+  $: console.debug("relations updated for", id, relations);
+
+  let thisAndParentIds = [id];
+  function findMissingRelations():
+    | { missing: string[][]; needed: string[][] }
+    | undefined {
     const thisNode = getNode(id);
     if (!thisNode) return;
-    const intersecting = getIntersectingNodes(thisNode, undefined);
-    const segment = intersecting.find((node) => node.type === "segment");
-    if (!segment) return;
-    const needsRelation = $templateInfoStore.nodes[segment.id].needsRelation;
-    if (!needsRelation) return;
 
     // Find all parents of thisNode to see e.g if entire group has a connection
     let thisNodeAndParents = [thisNode];
     let currentParent = thisNode.parentNode;
     while (currentParent) {
-      const parent = intersecting.find((node) => node.id === currentParent);
+      const parent = getNode(currentParent);
       if (!parent) break;
       currentParent = parent.parentNode;
       console.debug("found parent for", id, parent);
       thisNodeAndParents.push(parent);
     }
+    console.debug(
+      "NeededRelations found this node and parents",
+      thisNodeAndParents,
+    );
+    thisAndParentIds = thisNodeAndParents.map((node) => node.id);
+
+    const segment = thisNodeAndParents.find((node) => node.type === "segment");
+    if (!segment) return;
+
+    const needsRelation = $templateInfoStore.nodes[segment.id].needsRelation;
+    if (!needsRelation) return;
 
     const connections = getConnectedEdges(thisNodeAndParents, $edges);
     const connectedSegments: string[] = [];
@@ -49,26 +85,31 @@
     const missing = needsRelation.map((needed) =>
       needed.filter((segment) => !connectedSegments.includes(segment)),
     );
-    if (missing.some((needed) => needed.length === 0)) {
-      // We have fullfilled all requirements in one of the sets, no missing relations
-      return;
-    }
-    return missing;
+    return { missing, needed: needsRelation };
   }
 </script>
 
-{#if missingRelations}
+{#if relations}
+  {@const hasNeedRelations = relations.missing.some((set) => set.length === 0)}
   <div
     class="group/relation absolute right-0 top-0 size-2 -translate-y-1/2 translate-x-1/2 rounded-full"
   >
-    <InfoIcon size={12} class="rounded-full fill-orange-500" />
+    {#if hasNeedRelations}
+      <CheckCircle2Icon
+        size={12}
+        class="rounded-full fill-green-500 opacity-0 transition-opacity group-hover:opacity-100"
+      />
+    {:else}
+      <AlertCircleIcon size={12} class="rounded-full fill-orange-500" />
+    {/if}
     <div
       class="pointer-events-none absolute left-full top-full m-1 w-fit space-y-2 rounded bg-card p-2 text-card-foreground opacity-0 transition-opacity group-hover/relation:opacity-100"
     >
-      <nobr>Missing relations to:</nobr>
+      <nobr>Needed Relations:</nobr>
       <div>
-        {#each missingRelations as missing, i}
-          {#if i > 0}
+        {#each relations.needed as needed, setIndex}
+          {@const setFullfilled = relations.missing[setIndex].length === 0}
+          {#if setIndex > 0}
             <div
               class="grid items-center gap-1"
               style="grid-template-columns: 1fr auto 1fr;"
@@ -78,16 +119,27 @@
               <hr />
             </div>
           {/if}
-          {#each missing as m, i}
-            {@const info = $templateInfoStore.nodes[m]}
-            {#if i > 0}
-              <span> & </span>
+          <div class="flex flex-wrap items-center gap-2">
+            {#if setFullfilled}
+              <CheckCircle2Icon class="rounded-full bg-green-500" />
             {/if}
-            <nobr class="rounded px-1 text-sm {info.classes}">{info.title}</nobr
-            >
-          {/each}
+            {#each needed as m, i}
+              {@const info = $templateInfoStore.nodes[m]}
+              {@const fullfilled = relations.missing[setIndex].includes(m)}
+              {#if i > 0}
+                <span> & </span>
+              {/if}
+              <nobr
+                class="rounded px-1 text-sm {info.classes} {!fullfilled &&
+                  'opacity-50'}">{info.title}</nobr
+              >
+            {/each}
+          </div>
         {/each}
       </div>
+      <p class="text-sm text-muted-foreground">
+        * Dimmed names already have a relation to this item
+      </p>
     </div>
   </div>
 {/if}
