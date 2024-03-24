e99e0918aa5b863946f252f01c61a901fe639313
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-24 16:36:10 +0100

    Add segment color indicator and fit title better

--- src/lib/nodes/segment/SegmentDescPopover.svelte
+++ src/lib/nodes/segment/SegmentDescPopover.svelte
@@ -14,15 +14,22 @@
       <InfoIcon size={20} />
     </Popover.Trigger>
     <Popover.Content
-      class="space-y-2 overflow-y-auto pt-0"
+      class="overflow-y-auto p-0"
       align="end"
       side="left"
       fitViewport={true}
     >
-      <div class="sticky top-0 border-b bg-inherit pb-1 pt-4 text-lg">
+      <div
+        class="sticky top-0 flex items-center border-b bg-inherit px-4 pb-2 pt-2 text-lg"
+      >
+        <div
+          class="mr-2 inline-block size-4 flex-shrink-0 rounded-full {info.classes}"
+        ></div>
         {info.title}
       </div>
-      <svelte:component this={info.description} />
+      <div class="space-y-2 p-4 pt-2">
+        <svelte:component this={info.description} />
+      </div>
     </Popover.Content>
   </Popover.Root>
 {/if}
