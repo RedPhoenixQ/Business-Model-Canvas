cae75804516b11ff8b7ff8e1d6c740d344762726
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-25 12:22:01 +0100

    Throw error if loading a project with a newer version than the current

--- src/lib/project/version.ts
+++ src/lib/project/version.ts
@@ -6,5 +6,17 @@ export function migrateVersion(project: Project): Project {
   if (!project.version) project.version = 0;
   if (project.version === PROJECT_VERSION) return project;
   console.debug("migrating project", project);
+
+  if (project.version > PROJECT_VERSION) {
+    throw new Error(
+      "Project version is newer than this program. Please update the program.",
+      {
+        cause: {
+          savedVersion: project.version,
+          programVersion: PROJECT_VERSION,
+        },
+      },
+    );
+  }
   return project;
 }
