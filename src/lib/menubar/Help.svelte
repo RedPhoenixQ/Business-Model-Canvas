0ec117c0c0affaee6532bbe8ab1261d2ab8953bb
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-25 19:22:45 +0100

    Add Help with email and github link to Menubar

---
+++ src/lib/menubar/Help.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import * as Menubar from "$lib/components/ui/menubar";
+  import { BugIcon } from "lucide-svelte";
+  import GithubIcon from "../icons/GithubIcon.svelte";
+</script>
+
+<Menubar.Menu>
+  <Menubar.Trigger>Help</Menubar.Trigger>
+  <Menubar.Content>
+    <Menubar.Item class="gap-2" href="mailto:redphoenixq@gmail.com">
+      <BugIcon size={20} />
+      Report issue (email)
+    </Menubar.Item>
+    <Menubar.Item
+      class="gap-2"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Github project link"
+      href="https://github.com/RedPhoenixQ/Business-Model-Canvas"
+    >
+      <GithubIcon size={20} />
+      Github
+    </Menubar.Item>
+    <!-- svelte-ignore missing-declaration -->
+    <div class="text-small px-2 py-1 text-sm text-muted-foreground">
+      Version {APP_VERSION}
+    </div>
+  </Menubar.Content>
+</Menubar.Menu>
--- src/lib/menubar/Menubar.svelte
+++ src/lib/menubar/Menubar.svelte
@@ -4,12 +4,14 @@
   import Settings from "./Settings.svelte";
   import Debug from "./Debug.svelte";
   import Edit from "./Edit.svelte";
+  import Help from "./Help.svelte";
 </script>
 
 <Menubar.Root class="w-fit">
   <File />
   <Settings />
   <Edit />
+  <Help />
   {#if import.meta.env.DEV}
     <Debug />
   {/if}
