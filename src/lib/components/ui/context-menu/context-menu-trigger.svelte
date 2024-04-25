14423e19dc6b13f85887cf8ba75a6404c3e21314
RedPhoenixQ <steve.dark.03@gmail.com>
2024-04-25 15:47:27 +0200

    Ignore custom contextmenu when pressing shift

--- src/lib/components/ui/context-menu/context-menu-trigger.svelte
+++ src/lib/components/ui/context-menu/context-menu-trigger.svelte
@@ -16,6 +16,12 @@
   on:contextmenu={(event) => {
     if (event.detail.originalEvent.defaultPrevented) {
       event.preventDefault();
+    } else if (event.detail.originalEvent.shiftKey) {
+      // Ignore custom context menu if shift is pressed
+      event.preventDefault();
+      document.dispatchEvent(
+        new CustomEvent("custom-menu-close", { detail: "" }),
+      );
     } else {
       document.dispatchEvent(
         new CustomEvent("custom-menu-close", { detail: rootId }),
