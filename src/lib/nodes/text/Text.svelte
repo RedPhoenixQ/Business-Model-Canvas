3d73b8747fff53549f4c4a572d17e523bac07bd2
RedPhoenixQ <steve.dark.03@gmail.com>
2024-04-25 15:47:51 +0200

    Add contextmenu to TextNode

--- src/lib/nodes/text/Text.svelte
+++ src/lib/nodes/text/Text.svelte
@@ -5,6 +5,8 @@
   import type { TextNode } from ".";
   import { useNodeDataChange } from "..";
   import ResizeControl from "../ResizeControl.svelte";
+  import * as ContextMenu from "$lib/components/ui/context-menu";
+  import TextMenu from "./TextMenu.svelte";
 
   type $$Props = NodeProps<TextNode>;
 
@@ -14,29 +16,33 @@
   const nodeDataChange = useNodeDataChange(id, data);
 </script>
 
-<div
-  class="noscroll group h-full w-full rounded-md border-2 border-white border-opacity-25 bg-gray-700 bg-opacity-50 ring-blue-500 [.selected_&]:ring"
->
-  <ConnectionHandles />
-  <NeedsRelation
-    {id}
-    ignoredRelations={data.ignoredRelations}
-    on:ignoredChange={(e) => {
-      data.ignoredRelations = e.detail;
-      nodeDataChange(data, "ignoredRelations");
-    }}
-  />
-  <ResizeControl {id} minHeight={100} minWidth={150} />
+<ContextMenu.Root>
+  <TextMenu type="context-menu" {id} />
+  <ContextMenu.Trigger
+    class="group h-full w-full rounded-md border-2 border-white border-opacity-25 bg-gray-700 bg-opacity-50 ring-blue-500 [.selected_&]:ring"
+  >
+    <ConnectionHandles />
+    <NeedsRelation
+      {id}
+      ignoredRelations={data.ignoredRelations}
+      on:ignoredChange={(e) => {
+        data.ignoredRelations = e.detail;
+        nodeDataChange(data, "ignoredRelations");
+      }}
+    />
+    <ResizeControl {id} minHeight={100} minWidth={150} />
 
-  <textarea
-    class="h-full w-full resize-none rounded-md bg-background bg-opacity-75 p-2 text-foreground"
-    placeholder="Text here..."
-    on:copy|stopPropagation={() => {}}
-    on:cut|stopPropagation={() => {}}
-    on:paste|stopPropagation={() => {}}
-    on:wheel|stopPropagation={() => {}}
-    on:scroll|stopPropagation={() => {}}
-    on:change={() => nodeDataChange(data, "text")}
-    bind:value={data.text}
-  ></textarea>
-</div>
+    <textarea
+      class="h-full w-full resize-none rounded-md bg-background bg-opacity-75 p-2 text-foreground"
+      placeholder="Text here..."
+      autocomplete="off"
+      on:copy|stopPropagation={() => {}}
+      on:cut|stopPropagation={() => {}}
+      on:paste|stopPropagation={() => {}}
+      on:wheel|stopPropagation={() => {}}
+      on:scroll|stopPropagation={() => {}}
+      on:change={() => nodeDataChange(data, "text")}
+      bind:value={data.text}
+    ></textarea>
+  </ContextMenu.Trigger>
+</ContextMenu.Root>
---
+++ src/lib/nodes/text/TextMenu.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { type NodeProps } from "@xyflow/svelte";
+  import { type TextNode } from ".";
+  import { menu, type MenuType } from "$lib/components/custom/menu";
+  import MoveMenuPart from "../MoveMenuPart.svelte";
+  import DeleteNodeMenuPart from "../DeleteNodeMenuPart.svelte";
+  import CopyCutMenuPart from "$lib/copyPaste/CopyCutMenuPart.svelte";
+
+  type $$Props = Pick<NodeProps<TextNode>, "id"> & {
+    type: MenuType;
+  };
+
+  export let id: $$Props["id"];
+  export let type: $$Props["type"];
+
+  const { Content, Separator, Group } = menu(type);
+</script>
+
+<Content>
+  <MoveMenuPart {type} {id} />
+  <Separator />
+  <Group>
+    <CopyCutMenuPart {type} {id} />
+  </Group>
+  <Separator />
+  <DeleteNodeMenuPart {type} {id} />
+</Content>
