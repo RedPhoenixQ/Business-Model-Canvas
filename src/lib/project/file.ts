ea0a59ce8d98dd6c01fad80241e16906ec0486d4
RedPhoenixQ <steve.dark.03@gmail.com>
2024-04-29 02:19:11 +0200

    Create file hook with all file and save related functions

---
+++ src/lib/project/file.ts
@@ -0,0 +1,115 @@
+import {
+  showSaveFilePicker,
+  showOpenFilePicker,
+  FileSystemFileHandle,
+} from "native-file-system-adapter";
+import { handleStore, projectStore, useProject } from ".";
+import { derived, get, writable, type Writable } from "svelte/store";
+import { lastEntry, type HistoryEntry } from "./history";
+
+const types = [{ accept: { "application/json": [".json"] } }];
+
+const lastSavedEntryStore: Writable<HistoryEntry | undefined> = writable();
+
+export const savedStore = derived(
+  [lastEntry, lastSavedEntryStore, handleStore],
+  ([$lastEntry, $lastSaved, $handle]) => {
+    if ($lastEntry !== $lastSaved) {
+      return "unsaved";
+    } else if ($handle) {
+      return "file";
+    } else {
+      return "local";
+    }
+  },
+);
+
+export function useFile() {
+  const { toJSON, fromJSON } = useProject();
+
+  async function writeToHandle(json: string): Promise<boolean> {
+    const $handle = get(handleStore);
+    if (!$handle) return false;
+    try {
+      const writable = await $handle.createWritable();
+      const blob = new Blob([json], {
+        type: "application/json",
+      });
+      await blob.stream().pipeTo(writable);
+      return true;
+    } catch (err) {
+      console.error(err);
+      handleStore.set(undefined);
+      return false;
+    }
+  }
+
+  async function loadFromHandle(handle: FileSystemFileHandle) {
+    const file = await handle.getFile();
+    if (!file) return console.error("Could not access file from handle");
+    handleStore.set(handle);
+    fromJSON(await file.text());
+  }
+
+  async function saveLocal(json = toJSON()): Promise<boolean> {
+    localStorage.setItem("save", json);
+    lastSavedEntryStore.set(get(lastEntry));
+    return await writeToHandle(json);
+  }
+
+  async function loadLocal() {
+    const json = localStorage.getItem("save");
+    if (!json) return;
+    try {
+      fromJSON(json);
+    } catch (err) {
+      console.error("From local storage load", err);
+    }
+  }
+
+  async function saveFile(json: string = toJSON()) {
+    if (!(await writeToHandle(json))) {
+      saveFileAs(json);
+    }
+  }
+
+  async function saveFileAs(json: string = toJSON()) {
+    const name = get(projectStore).name;
+    const handle = await showSaveFilePicker({
+      _preferPolyfill: false,
+      _name: `${name ?? "unnamed"}.json`,
+      suggestedName: `${name ?? "unnamed"}.json`,
+      excludeAcceptAllOption: true,
+      types,
+      //@ts-expect-error: Exists in the native api on chrome
+      startIn: "documents",
+      id: "FileHandler",
+    });
+    handleStore.set(handle);
+    saveLocal(json);
+  }
+
+  async function openFile() {
+    const [handle] = await showOpenFilePicker({
+      _preferPolyfill: false,
+      excludeAcceptAllOption: true,
+      accepts: [{ mimeTypes: ["application/json"] }],
+      multiple: false,
+      //@ts-expect-error: Exists in the native api on chrome
+      startIn: "documents",
+      types,
+      id: "FileHandler",
+    });
+    if (!handle) return console.error("No file choosen", handle);
+    loadFromHandle(handle);
+  }
+
+  return {
+    loadFromHandle,
+    saveLocal,
+    loadLocal,
+    openFile,
+    saveFile,
+    saveFileAs,
+  };
+}
